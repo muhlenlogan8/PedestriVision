fe7e6fc927d0d19ac954ab3778c053a4b6379459
muhlenlogan8 <muhlenlogan8@gmail.com>
2025-11-04 17:11:24 -0500

    Getting expo app to build

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, View } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
 import "./global.css";
 
 export default function App() {
 	return (
-		<>
-			<HomeScreen />
+		<SafeAreaView className="flex-1">
+			<View className="flex-1 bg-gray-900">
+				<HomeScreen />
+			</View>
 			<StatusBar style="light" />
-		</>
+		</SafeAreaView>
 	);
 }
